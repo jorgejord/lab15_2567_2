cccb0f4bfaf03130fe6abee9345515f2229fd8ee
Jorge <57448609+jorgejord@users.noreply.github.com>
2025-01-31 17:51:39 +0700

    Add files via upload

---
+++ lab15_5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+void shuffle(int *,int *,int *,int *);
+
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(&a,&b,&c,&d);
+	    cout << a << " " << b << " " << c << " " << d << endl;
+	}
+	
+	return 0;
+}
+
+//Do not modify the code above this line
+//Write definition of shuffle() using pointer after this line. 
+void shuffle(int *a ,int *b ,int *c ,int *d){
+	int A[] = {*a,*b,*c,*d};
+	for(int i = 0;i < 3 ; i++){
+		int tempt = rand()%4;
+		int tempt2 = A[i];
+		A[i] = A[tempt];
+		A[tempt] = tempt2 ;
+
+	}
+	*a = A[0];
+	*b = A[1];
+	*c = A[2];
+	*d = A[3];
+}
